54a8805f69e9f512cbe304cc1ceceeaf19735387
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-09-11 02:19:02 +0000

    git-svn-id: https://insvr/svn/svn/spring3@280 e87f34ec-376e-c441-8d07-ae210fbb51ab

---
+++ src/com/mftour/spring/Dao/ICrowdfundingDao.java
@@ -0,0 +1,18 @@
+package com.mftour.spring.Dao;
+
+import java.util.List;
+
+import com.mftour.spring.model.TCrowdfunding;
+import com.mftour.spring.model.TCrowdfundingCommodity;
+import com.mftour.spring.util.Page;
+
+public interface  ICrowdfundingDao {
+	
+	
+	public List<TCrowdfunding> getCrowdfunding(Page page,TCrowdfunding crowdfunding)throws Exception;
+	
+	public TCrowdfunding getCrowdfundingById(Long id);
+	
+	 public List<TCrowdfundingCommodity> queryTCrowdfundingCommodityByNumber(String  projectNumber)throws Exception;
+
+}
