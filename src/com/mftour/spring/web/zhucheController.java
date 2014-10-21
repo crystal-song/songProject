e5ec68dbab0976b2604ec6ce0c5030aae1c6002f
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-10-21 09:11:37 +0000

    git-svn-id: https://insvr/svn/svn/spring3@526 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/web/zhucheController.java
+++ src/com/mftour/spring/web/zhucheController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mftour.spring.model.TNews;
 import com.mftour.spring.model.TProduct;
+import com.mftour.spring.model.TRegisterYeePay;
 import com.mftour.spring.model.TUser;
+import com.mftour.spring.service.IGateService;
 import com.mftour.spring.service.IProductService;
 import com.mftour.spring.service.IUserService;
 import com.mftour.spring.service.IptopService;
@@ -30,6 +32,9 @@ public class zhucheController {
 	
 	@Autowired
     private IUserService userService;
+	
+	@Autowired
+    private IGateService gateService;
 	
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.GET})
 	public String helloWorld(Model model) throws Exception {
@@ -58,9 +63,12 @@ public class zhucheController {
 	@RequestMapping(value = "/account",method = {RequestMethod.POST, RequestMethod.GET})
 	public String account(Model model,HttpServletRequest request) throws Exception {
 		 Object o= request.getSession().getAttribute("name");
+		
 		 if(o!=null){
+			 System.out.println("zhanghu"+o.toString());
 			 TUser user1 = userService.getUserByAccount(o.toString());
-				
+				TRegisterYeePay registerYeePay1= gateService.queryTRegisterYeePayByName(user1.getName()).get(0);
+				model.addAttribute("registerYeePay1", registerYeePay1);	
 				model.addAttribute("user1", user1);
 				return "user-info";
 			 
