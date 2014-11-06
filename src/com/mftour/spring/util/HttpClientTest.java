49f9936dd38ef4b4142fa9b6918d8c1eb410fc5b
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-11-06 10:54:54 +0000

    git-svn-id: https://insvr/svn/svn/spring3@614 e87f34ec-376e-c441-8d07-ae210fbb51ab

---
+++ src/com/mftour/spring/util/HttpClientTest.java
@@ -0,0 +1,69 @@
+package com.mftour.spring.util;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientTest {
+	
+	
+	public String postForm(String service,String url,String s) {  
+        // 创建默认的httpClient实例.    
+        CloseableHttpClient httpclient = HttpClients.createDefault();  
+        // 创建httppost    
+        HttpPost httppost = new HttpPost(url);  
+        // 创建参数队列    
+        List formparams = new ArrayList();  
+        formparams.add(new BasicNameValuePair("service", service));  
+        formparams.add(new BasicNameValuePair("req", s));  
+        formparams.add(new BasicNameValuePair("sign", "ddd"));
+        UrlEncodedFormEntity uefEntity;  
+        try {  
+            uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");  
+            httppost.setEntity(uefEntity);  
+            System.out.println("executing request " + httppost.getURI());  
+            CloseableHttpResponse response = httpclient.execute(httppost);  
+            try {  
+                HttpEntity entity = response.getEntity();  
+                if (entity != null) {  
+                    System.out.println("--------------------------------------");  
+                   /* System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    String name=EntityUtils.toString(entity, "UTF-8");*/
+                    
+                    System.out.println("--------------------------------------");  
+                   
+                    return EntityUtils.toString(entity, "UTF-8");
+                }  
+            } finally {  
+                response.close();  
+            }  
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (UnsupportedEncodingException e1) {  
+            e1.printStackTrace(); 
+        } catch (IOException e) {  
+            e.printStackTrace();    
+        } finally {  
+            // 关闭连接,释放资源    
+            try {  
+                httpclient.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+        return null;
+    }  
+  
+
+}
