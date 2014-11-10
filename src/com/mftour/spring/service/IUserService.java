972ca4c3807de211f692d2b5227fb8df53ebc361
songxiaoyue <songxiaoyue@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-11-10 09:52:51 +0000

    git-svn-id: https://insvr/svn/svn/spring3@714 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/service/IUserService.java
+++ src/com/mftour/spring/service/IUserService.java
@@ -6,6 +6,7 @@ import com.mftour.spring.model.TUser;
 
 public interface IUserService {
 	public TUser getUserByAccount(String account) throws Exception;
+	public TUser getUserByEmail(String email) throws Exception;
 	
 	public void addOrUpdate(TUser user) throws Exception;
 	
