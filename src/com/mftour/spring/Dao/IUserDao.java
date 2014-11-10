b54f41fba71fa557bedfe9d0917d499ebc719d80
songxiaoyue <songxiaoyue@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-11-10 09:52:29 +0000

    git-svn-id: https://insvr/svn/svn/spring3@712 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/Dao/IUserDao.java
+++ src/com/mftour/spring/Dao/IUserDao.java
@@ -8,6 +8,8 @@ public interface IUserDao {
 	
 	public List getUserByAccount(String account) throws Exception;
 	
+	public List getUserByEmail(String email) throws Exception;
+	
 	public void addOrUpdate(TUser user) throws Exception;
 	
 	public List<TUser> getName(String name)throws Exception;
