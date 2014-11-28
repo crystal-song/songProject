517791ef484576bc444667f661950ffdf6d27cf5
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-11-28 02:00:40 +0000

    git-svn-id: https://insvr/svn/svn/spring3@898 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/service/Impl/IGateServiceImpl.java
+++ src/com/mftour/spring/service/Impl/IGateServiceImpl.java
@@ -1,3 +1,4 @@
+
 package com.mftour.spring.service.Impl;
 
 import java.util.List;
@@ -7,70 +8,168 @@ import org.springframework.stereotype.Service;
 
 import com.mftour.spring.Dao.ICrowdfundingDao;
 import com.mftour.spring.Dao.IGateDao;
+import com.mftour.spring.model.TBinding;
+import com.mftour.spring.model.TBindingSucceed;
 import com.mftour.spring.model.TDrawMoney;
+import com.mftour.spring.model.TDrawMoneySucceed;
 import com.mftour.spring.model.TEstablishmentNotify;
 import com.mftour.spring.model.TEstablishmentRegistration;
+import com.mftour.spring.model.TLoansucceed;
 import com.mftour.spring.model.TRecharge;
+import com.mftour.spring.model.TRechargeSucceed;
 import com.mftour.spring.model.TRegisterYeePay;
 import com.mftour.spring.model.TTransferInfo;
 import com.mftour.spring.model.TTransferSucceed;
 import com.mftour.spring.model.TYeePay;
 import com.mftour.spring.service.IGateService;
-import com.mftour.spring.util.Page;
 
 @Service("gateService")
-public class IGateServiceImpl implements IGateService {
+public class IGateServiceImpl  implements  IGateService {
 
+	
 	@Autowired
-	private IGateDao gateDao;
-
+    private IGateDao gateDao;
+	
+	
 	@Override
 	public void addOrUpdate(TYeePay YeePay) throws Exception {
 		gateDao.addOrUpdate(YeePay);
-
+		
 	}
+
 
 	@Override
 	public void addOrUpdateRegisterYeePay(TRegisterYeePay registerYeePay)
 			throws Exception {
 		gateDao.addOrUpdateRegisterYeePay(registerYeePay);
-
+		
 	}
+
 
 	@Override
 	public List<TRegisterYeePay> queryTRegisterYeePayByName(String name)
 			throws Exception {
-
+		
 		return gateDao.queryTRegisterYeePayByName(name);
 	}
+
 
 	@Override
 	public List<TRegisterYeePay> queryTRegisterYeePayByNumber(String Number)
 			throws Exception {
-
+		
 		return gateDao.queryTRegisterYeePayByNumber(Number);
 	}
+
 
 	@Override
 	public void addOrUpdateTTransferSucceed(TTransferSucceed transferSucceed)
 			throws Exception {
-		gateDao.addOrUpdateTTransferSucceed(transferSucceed);
-
+			gateDao.addOrUpdateTTransferSucceed(transferSucceed);
+		
 	}
+
 
 	@Override
 	public void addOrUpdateTTransferInfo(TTransferInfo TTransferInfo)
 			throws Exception {
 		gateDao.addOrUpdateTTransferInfo(TTransferInfo);
-
+		
 	}
+
 
 	@Override
 	public List<TTransferInfo> queryTTransferInfoByNumber(String Number)
 			throws Exception {
 		return gateDao.queryTTransferInfoByNumber(Number);
-
+		
 	}
+
+
+	@Override
+	public void addOrUpdateTEstablishmentRegistration(
+			TEstablishmentRegistration establishmentRegistration)
+			throws Exception {
+		gateDao.addOrUpdateTEstablishmentRegistration(establishmentRegistration);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTEstablishmentNotify(
+			TEstablishmentNotify establishmentNotify) throws Exception {
+		gateDao.addOrUpdateTEstablishmentNotify(establishmentNotify);
+		
+	}
+
+
+	@Override
+	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByNumber(
+			String Number) throws Exception {
+		
+		return gateDao.queryTEstablishmentRegistrationByNumber(Number);
+	}
+
+
+	@Override
+	public void addOrUpdateTBindingSucceed(TBindingSucceed bindingSucceed)
+			throws Exception {
+		gateDao.addOrUpdateTBindingSucceed(bindingSucceed);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTBinding(TBinding binding) throws Exception {
+		gateDao.addOrUpdateTBinding(binding);
+		
+	}
+
+
+	@Override
+	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByName(
+			String name) throws Exception {
+		return gateDao.queryTEstablishmentRegistrationByName(name);
+	}
+
+
+	@Override
+	public void addOrUpdateTDrawMoney(TDrawMoney drawMoney) throws Exception {
+		gateDao.addOrUpdateTDrawMoney(drawMoney);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTDrawMoneySucceed(TDrawMoneySucceed drawMoneySucceed)
+			throws Exception {
+		gateDao.addOrUpdateTDrawMoneySucceed(drawMoneySucceed);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTLoansucceed(TLoansucceed loansucceed)
+			throws Exception {
+		gateDao.addOrUpdateTLoansucceed(loansucceed);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTRecharge(TRecharge recharge) throws Exception {
+		gateDao.addOrUpdateTRecharge(recharge);
+		
+	}
+
+
+	@Override
+	public void addOrUpdateTRechargeSucceed(TRechargeSucceed rechargeSucceed)
+			throws Exception {
+		gateDao.addOrUpdateTRechargeSucceed(rechargeSucceed);
+		
+	}
+	
 	public List<TTransferInfo> queryAllTransRecord(String platformUserNo)
 			throws Exception {
 		return gateDao.queryAllTransRecord(platformUserNo);
@@ -86,26 +185,4 @@ public class IGateServiceImpl implements IGateService {
 			throws Exception{
 		return gateDao.AllTransRecord(pageNo, platformUserNo);
 	}
-	@Override
-	public void addOrUpdateTEstablishmentRegistration(
-			TEstablishmentRegistration establishmentRegistration)
-			throws Exception {
-		gateDao.addOrUpdateTEstablishmentRegistration(establishmentRegistration);
-
-	}
-
-	@Override
-	public void addOrUpdateTEstablishmentNotify(
-			TEstablishmentNotify establishmentNotify) throws Exception {
-		gateDao.addOrUpdateTEstablishmentNotify(establishmentNotify);
-
-	}
-
-	@Override
-	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByNumber(
-			String Number) throws Exception {
-
-		return gateDao.queryTEstablishmentRegistrationByNumber(Number);
-	}
-
 }
