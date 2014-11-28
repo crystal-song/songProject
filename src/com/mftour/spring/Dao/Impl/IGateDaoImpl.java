ef8a025482eb221c60d2522164efe01b9599249b
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-11-28 02:03:07 +0000

    git-svn-id: https://insvr/svn/svn/spring3@899 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/Dao/Impl/IGateDaoImpl.java
+++ src/com/mftour/spring/Dao/Impl/IGateDaoImpl.java
@@ -1,4 +1,4 @@
-package com.mftour.spring.Dao.Impl;
+	package com.mftour.spring.Dao.Impl;
 
 import java.util.List;
 
@@ -7,22 +7,26 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.mftour.spring.Dao.IGateDao;
+import com.mftour.spring.model.TBinding;
+import com.mftour.spring.model.TBindingSucceed;
 import com.mftour.spring.model.TDrawMoney;
+import com.mftour.spring.model.TDrawMoneySucceed;
 import com.mftour.spring.model.TEstablishmentNotify;
 import com.mftour.spring.model.TEstablishmentRegistration;
+import com.mftour.spring.model.TLoansucceed;
 import com.mftour.spring.model.TRecharge;
+import com.mftour.spring.model.TRechargeSucceed;
 import com.mftour.spring.model.TRegisterYeePay;
 import com.mftour.spring.model.TTransferInfo;
 import com.mftour.spring.model.TTransferSucceed;
 import com.mftour.spring.model.TYeePay;
-import com.mftour.spring.util.Page;
 
 @Repository("gateDao")
-public class IGateDaoImpl extends HibernateDaoSupport implements IGateDao {
+public class IGateDaoImpl  extends HibernateDaoSupport  implements  IGateDao {
 
 	@Override
 	public void addOrUpdate(TYeePay YeePay) throws Exception {
-
+		
 		getHibernateTemplate().saveOrUpdate(YeePay);
 	}
 
@@ -30,7 +34,7 @@ public class IGateDaoImpl extends HibernateDaoSupport implements IGateDao {
 	public void addOrUpdateRegisterYeePay(TRegisterYeePay registerYeePay)
 			throws Exception {
 		getHibernateTemplate().saveOrUpdate(registerYeePay);
-
+		
 	}
 
 	@Override
@@ -55,14 +59,14 @@ public class IGateDaoImpl extends HibernateDaoSupport implements IGateDao {
 	public void addOrUpdateTTransferSucceed(TTransferSucceed transferSucceed)
 			throws Exception {
 		getHibernateTemplate().saveOrUpdate(transferSucceed);
-
+		
 	}
 
 	@Override
 	public void addOrUpdateTTransferInfo(TTransferInfo TTransferInfo)
 			throws Exception {
 		getHibernateTemplate().saveOrUpdate(TTransferInfo);
-
+		
 	}
 
 	@Override
@@ -73,6 +77,86 @@ public class IGateDaoImpl extends HibernateDaoSupport implements IGateDao {
 		query.setParameter("requestNo", Number);
 		return query.list();
 	}
+
+	@Override
+	public void addOrUpdateTEstablishmentRegistration(
+			TEstablishmentRegistration establishmentRegistration)
+			throws Exception {
+		getHibernateTemplate().saveOrUpdate(establishmentRegistration);
+		
+	}
+
+	@Override
+	public void addOrUpdateTEstablishmentNotify(
+			TEstablishmentNotify establishmentNotify) throws Exception {
+		getHibernateTemplate().saveOrUpdate(establishmentNotify);
+		
+	}
+
+	@Override
+	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByNumber(
+			String Number) throws Exception {
+		String hq = "from TEstablishmentRegistration establishmentRegistration where establishmentRegistration.platformUserNo = :platformUserNo";
+		Query query = getSession().createQuery(hq);
+		query.setParameter("platformUserNo", Number);
+		return query.list();
+	}
+
+	@Override
+	public void addOrUpdateTBindingSucceed(TBindingSucceed bindingSucceed)
+			throws Exception {
+		getHibernateTemplate().saveOrUpdate(bindingSucceed);
+		
+	}
+
+	@Override
+	public void addOrUpdateTBinding(TBinding binding) throws Exception {
+		getHibernateTemplate().saveOrUpdate(binding);
+		
+	}
+
+	@Override
+	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByName(
+			String name) throws Exception {
+		String hq = "from TEstablishmentRegistration establishmentRegistration where establishmentRegistration.platformUserNo = :platformUserNo";
+		Query query = getSession().createQuery(hq);
+		query.setParameter("platformUserNo", name);
+		return query.list();
+	}
+
+	@Override
+	public void addOrUpdateTDrawMoney(TDrawMoney drawMoney) throws Exception {
+		getHibernateTemplate().saveOrUpdate(drawMoney);
+		
+	}
+
+	@Override
+	public void addOrUpdateTDrawMoneySucceed(TDrawMoneySucceed drawMoneySucceed)
+			throws Exception {
+		getHibernateTemplate().saveOrUpdate(drawMoneySucceed);
+		
+	}
+
+	@Override
+	public void addOrUpdateTLoansucceed(TLoansucceed loansucceed)
+			throws Exception {
+		getHibernateTemplate().saveOrUpdate(loansucceed);
+		
+	}
+
+	@Override
+	public void addOrUpdateTRecharge(TRecharge recharge) throws Exception {
+		getHibernateTemplate().saveOrUpdate(recharge);
+		
+	}
+
+	@Override
+	public void addOrUpdateTRechargeSucceed(TRechargeSucceed rechargeSucceed)
+			throws Exception {
+		getHibernateTemplate().saveOrUpdate(rechargeSucceed);
+		
+	}
+	
 	public List<TTransferInfo> queryAllTransRecord(String platformUserNo)throws Exception {
 		String hq = "from TTransferInfo transferInfo where transferInfo.platformUserNo=:platformUserNo order by transferInfo.transDate asc";
 		Query query = getSession().createQuery(hq);
@@ -104,29 +188,6 @@ public class IGateDaoImpl extends HibernateDaoSupport implements IGateDao {
 		query.setParameter("platformUserNo3", platformUserNo);
 		return query.list();
 	}
-
-	@Override
-	public void addOrUpdateTEstablishmentRegistration(
-			TEstablishmentRegistration establishmentRegistration)
-			throws Exception {
-		getHibernateTemplate().saveOrUpdate(establishmentRegistration);
-
-	}
-
-	@Override
-	public void addOrUpdateTEstablishmentNotify(
-			TEstablishmentNotify establishmentNotify) throws Exception {
-		getHibernateTemplate().saveOrUpdate(establishmentNotify);
-
-	}
-
-	@Override
-	public List<TEstablishmentRegistration> queryTEstablishmentRegistrationByNumber(
-			String Number) throws Exception {
-		String hq = "from TEstablishmentRegistration establishmentRegistration where establishmentRegistration.requestNo = :requestNo";
-		Query query = getSession().createQuery(hq);
-		query.setParameter("requestNo", Number);
-		return query.list();
-	}
-
+	
 }
+	
