a22fa98882815be3cf6cf9b89861e4b16ff92723
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-01 13:42:13 +0000

    git-svn-id: https://insvr/svn/svn/spring3@982 e87f34ec-376e-c441-8d07-ae210fbb51ab

---
+++ src/com/mftour/spring/model/TTransferNotify.java
@@ -0,0 +1,51 @@
+package com.mftour.spring.model;
+
+public class TTransferNotify implements java.io.Serializable{
+	private static final long serialVersionUID = -8032736664676548825L;
+	
+	   private Long id;
+	    private String platformNo;    //商户编号
+	    private String bizType;     //服务名称
+	    private String code;   //返回码
+	    private String message; //描述
+	    private String requestNo;  //请求流水号
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getPlatformNo() {
+			return platformNo;
+		}
+		public void setPlatformNo(String platformNo) {
+			this.platformNo = platformNo;
+		}
+		public String getBizType() {
+			return bizType;
+		}
+		public void setBizType(String bizType) {
+			this.bizType = bizType;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public String getRequestNo() {
+			return requestNo;
+		}
+		public void setRequestNo(String requestNo) {
+			this.requestNo = requestNo;
+		}
+	
+	
+
+}
