ec06eef9c45bbd1128762942a4a0a6c9fd5403e6
songxiaoyue <songxiaoyue@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-07 05:25:35 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1149 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/Dao/ITransNoticeDao.java
+++ src/com/mftour/spring/Dao/ITransNoticeDao.java
@@ -10,6 +10,12 @@ public interface ITransNoticeDao {
 	public void addOrUpdate(TTransNotice transnotice) throws Exception;
 	public List<TTransNotice> queryTransNoticeByName(String username)
 			throws Exception;
+	public List<TTransNotice> queryTransNoticeByNameAndtype(String username,String type)
+			throws Exception;
+	public List<TTransNotice> queryTransNoticeByNameAndtypes(String username,String type,String noticeType)
+			throws Exception;
+	public void delTransNoticeByName(String username)
+			throws Exception;
 
 }
 	 
