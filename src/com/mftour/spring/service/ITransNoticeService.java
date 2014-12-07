6d9938f305f041abd7cd89c980eae2bf80927424
songxiaoyue <songxiaoyue@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-07 05:22:58 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1148 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/service/ITransNoticeService.java
+++ src/com/mftour/spring/service/ITransNoticeService.java
@@ -10,5 +10,10 @@ public interface ITransNoticeService {
 	public void addOrUpdate(TTransNotice transnotice) throws Exception;
 	public List<TTransNotice> queryTransNoticeByName(String username)
 			throws Exception;
-	
+	public List<TTransNotice> queryTransNoticeByNameAndtype(String username,String type)
+			throws Exception;
+	public void delTransNoticeByName(String username)
+			throws Exception;
+	public List<TTransNotice> queryTransNoticeByNameAndtypes(String username,String type,String noticeType)
+			throws Exception;
 }
