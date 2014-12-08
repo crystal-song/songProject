13612ebfc08dff6ad0316b4f353a43be06f4c174
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-08 09:51:04 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1209 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/Dao/IGateDao.java
+++ src/com/mftour/spring/Dao/IGateDao.java
@@ -3,17 +3,22 @@
 import java.util.List;
 
 import com.mftour.spring.model.TBinding;
+import com.mftour.spring.model.TBindingNotify;
 import com.mftour.spring.model.TBindingSucceed;
 import com.mftour.spring.model.TDrawMoney;
+import com.mftour.spring.model.TDrawMoneyNotify;
 import com.mftour.spring.model.TDrawMoneySucceed;
 import com.mftour.spring.model.TEstablishmentNotify;
 import com.mftour.spring.model.TEstablishmentRegistration;
 import com.mftour.spring.model.TInvestmentInfo;
 import com.mftour.spring.model.TLoansucceed;
 import com.mftour.spring.model.TRecharge;
+import com.mftour.spring.model.TRechargeNotify;
 import com.mftour.spring.model.TRechargeSucceed;
+import com.mftour.spring.model.TRegisterNotify;
 import com.mftour.spring.model.TRegisterYeePay;
 import com.mftour.spring.model.TTransferInfo;
+import com.mftour.spring.model.TTransferNotify;
 import com.mftour.spring.model.TTransferSucceed;
 import com.mftour.spring.model.TYeePay;
 import com.mftour.spring.util.Page;
@@ -62,6 +67,21 @@ public interface IGateDao {
 	 public List<TInvestmentInfo> queryTInvestmentInfoByName(String name)throws Exception;
 	 
 	 public List<TTransferInfo> queryTTransferInfoByEnterpriseNumber(String  enterpriseNumber)throws Exception;
+	 
+	 public void addOrUpdateTRegisterNotify(TRegisterNotify registerNotify) throws Exception;
+	 
+	 public List<TRegisterNotify> queryTRegisterNotifyByName(String name)throws Exception;
+	 
+	 public void addOrUpdateTRechargeNotify(TRechargeNotify rechargeNotify) throws Exception;
+	 
+	 public void addOrUpdateTBindingNotify(TBindingNotify bindingNotify) throws Exception;
+	 
+	 public void addOrUpdateTTransferNotify(TTransferNotify transferNotify) throws Exception;
+	 
+	 public void addOrUpdateTDrawMoneyNotify(TDrawMoneyNotify drawMoneyNotify) throws Exception;
+	 
+	 
+	 
 	 
 	 public List queryAllTransRecord(Page page,String sql,String platformUserNo)
 				throws Exception;
