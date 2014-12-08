497a76505b0c496d15464b55b4778a2d3fade1aa
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-08 03:18:31 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1192 e87f34ec-376e-c441-8d07-ae210fbb51ab

---
+++ src/com/mftour/spring/util/File.java
@@ -0,0 +1,34 @@
+package com.mftour.spring.util;
+
+public class File {
+    private String platformNo;    
+    private String callbackUrl;   
+    private String notifyUrl;
+    private String url;
+	public String getPlatformNo() {
+		return platformNo;
+	}
+	public void setPlatformNo(String platformNo) {
+		this.platformNo = platformNo;
+	}
+	public String getCallbackUrl() {
+		return callbackUrl;
+	}
+	public void setCallbackUrl(String callbackUrl) {
+		this.callbackUrl = callbackUrl;
+	}
+	public String getNotifyUrl() {
+		return notifyUrl;
+	}
+	public void setNotifyUrl(String notifyUrl) {
+		this.notifyUrl = notifyUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	} 
+	
+   
+}
