75c420c16857159842e38c276e754437576d0acc
songxiaoyue <songxiaoyue@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-09 09:57:33 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1278 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/util/RandomCode.java
+++ src/com/mftour/spring/util/RandomCode.java
@@ -94,4 +94,16 @@ public class RandomCode extends HttpServlet {
 			throws ServletException, IOException {
 		this.doGet(request, response);
 	}
+	 //注册时生成随机码
+	 public static String getRandomString(int length) { //length表示生成字符串的长度
+		    String base = "abcdefghijklmnopqrstuvwxyz0123456789";   
+		    Random random = new Random();   
+		    StringBuffer sb = new StringBuffer();   
+		    for (int i = 0; i < length; i++) {   
+		        int number = random.nextInt(base.length());   
+		        sb.append(base.charAt(number));   
+		    }   
+		    return sb.toString();   
+		 }   
+
 }
