3f546b387b0bcca4b0e91075b422cb7a24320b6b
liukai <liukai@e87f34ec-376e-c441-8d07-ae210fbb51ab>
2014-12-10 05:50:30 +0000

    git-svn-id: https://insvr/svn/svn/spring3@1298 e87f34ec-376e-c441-8d07-ae210fbb51ab

--- src/com/mftour/spring/web/GateController.java
+++ src/com/mftour/spring/web/GateController.java
@@ -546,10 +546,11 @@ public class GateController  {
 				 if(code!=null && code.equals("1")){
  		 /* if(registerYeePay1.getPlatformUserNo()!=null&&registerYeePay1.getCode().equals("1")){*/
 			     /* if(li.get(0).getCode().equals("1")||list.get(0).getCode().equals("1")){*/
+					 if(li!=null){
 					 registerYeePay1=li.get(0);
 	        	  model.addAttribute("registerYeePay1", registerYeePay1);
 	        	  return "chongzhi";
- 		  /* }*/
+ 		  /* }*/}
 				 }
 			 }/*else{
 				 TUser user=userService.getUserByAccount(o.toString());
